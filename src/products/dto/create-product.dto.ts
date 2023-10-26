b3745f3866832b01a4fbc85ec379095807a58695
Paul Llanque <llanquepol@gmail.com>
2023-10-25 22:58:42 -0500

    [ADD] create-product.dto added - class validator

---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,41 @@
+import {
+  IsString,
+  MinLength,
+  IsNumber,
+  IsPositive,
+  IsOptional,
+  IsInt,
+  IsArray,
+  IsIn,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @MinLength(3)
+  title: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  price?: number;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsString()
+  @IsOptional()
+  slug?: string;
+
+  @IsInt()
+  @IsPositive()
+  @IsOptional()
+  stock?: number;
+
+  @IsString({ each: true })
+  @IsArray()
+  sizes: string[];
+
+  @IsIn(['men', 'women', 'kid', 'unisex'])
+  gender: string;
+}
