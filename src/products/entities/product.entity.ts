40713559005d9abe6947214812303eb4584a4eb7
Paul Llanque <llanquepol@gmail.com>
2023-10-25 22:57:36 -0500

    [ADD] entity added

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,28 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text', { unique: true })
+  title: string;
+
+  @Column('numeric', { default: 0 })
+  price: number;
+
+  @Column({ type: 'text', nullable: true })
+  descrition: string;
+
+  @Column('text', { unique: true })
+  slug: string;
+
+  @Column('int', { default: 0 })
+  stock: number;
+
+  @Column('text', { array: true })
+  sizes: string[];
+
+  @Column('text')
+  gender: string;
+}
