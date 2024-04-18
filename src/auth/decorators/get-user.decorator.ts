7947cb05e2b19fa41b8fa8ce2c4163d1daf83c2a
Paul Llanque <llanquepol@gmail.com>
2024-04-18 18:59:07 -0500

    [ADD] get user from decorator

---
+++ src/auth/decorators/get-user.decorator.ts
@@ -0,0 +1,16 @@
+import {
+  createParamDecorator,
+  ExecutionContext,
+  InternalServerErrorException,
+} from '@nestjs/common';
+
+export const GetUser = createParamDecorator((data, ctx: ExecutionContext) => {
+  const req = ctx.switchToHttp().getRequest();
+  const user = req.user;
+  if (!user)
+    throw new InternalServerErrorException(
+      'User not found (GetUser Decorator)',
+    );
+
+  return data ? user[data] : user;
+});
