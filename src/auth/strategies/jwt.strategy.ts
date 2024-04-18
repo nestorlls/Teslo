ebc57c78ee37cbb7838ce0589bbba4fe6c0fcf14
Paul Llanque <llanquepol@gmail.com>
2024-04-17 23:02:54 -0500

    [ADD] jwt strategy to validate is exists

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,31 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { Repository } from 'typeorm';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+import { IJwtPayload } from '../interfaces/jwt-payload.interface';
+import { User } from '../entities/user.entity';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    configService: ConfigService,
+  ) {
+    super({
+      secretOrKey: configService.get('JWT_SECRET'),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+  async validate(payload: IJwtPayload): Promise<User> {
+    const { email } = payload;
+    const user = await this.userRepository.findOneBy({ email });
+    if (!user) throw new UnauthorizedException('Token not valid');
+    if (!user.isActive) throw new UnauthorizedException('User not active');
+    return user;
+  }
+}
