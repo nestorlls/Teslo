87344c5654c8f88afd3e6772579e164870fcce21
Paul Llanque <llanquepol@gmail.com>
2024-04-17 23:01:51 -0500

    [ADD] user entity

---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,38 @@
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text', { unique: true })
+  email: string;
+
+  @Column('text', { select: false })
+  password: string;
+
+  @Column('text', { nullable: false, select: false })
+  fullName: string;
+
+  @Column('boolean', { default: true })
+  isActive: boolean;
+
+  @Column('text', { array: true, default: ['user'] })
+  roles: string[];
+
+  @BeforeInsert()
+  checkFieldsBeforeInsert() {
+    this.email = this.email.toLowerCase().trim();
+  }
+
+  @BeforeUpdate()
+  checkFieldsBeforeUpdate() {
+    this.checkFieldsBeforeInsert();
+  }
+}
