6f0be0ba9551753ffb74cd9f28801d9a0d943809
Paul Llanque <llanquepol@gmail.com>
2024-04-18 19:25:56 -0500

    [UPDATE] adding AuthModule on SeedModule

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -3,10 +3,11 @@ import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
 import { ProductsModule } from 'src/products/products.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [SeedController],
   providers: [SeedService],
-  imports: [ProductsModule],
+  imports: [ProductsModule, AuthModule],
 })
 export class SeedModule {}
